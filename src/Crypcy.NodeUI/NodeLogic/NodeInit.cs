362d8558b06753dfe4e062afb0fddf0222f78348
d.marchuk <d.marchuk@godeltech.com>
2022-12-26 00:32:35 +0300

    init NodeUI project

---
+++ src/Crypcy.NodeUI/NodeLogic/NodeInit.cs
@@ -0,0 +1,29 @@
+﻿using Crypcy.ApplicationCore.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypcy.NodeUI.NodeLogic
+{
+    internal class NodeInit : IUserInterface
+    {
+        public event Action<string, string> OnSendMessageRequest;
+
+        public void NodeConnectedNotification(string node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NodeDiconnectedNotification(string node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMessage(string node, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Crypcy.NodeUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Crypcy.NodeUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
